6f7388f254409d0c24a544d1de05b31adb17afe1
wllmbr <wllmbr@hotmail.com>
2019-11-09 14:36:28 -0700

    Updated ignore

--- src/galactic_empires/perform_game_tick.cpp
+++ src/galactic_empires/perform_game_tick.cpp
@@ -2,37 +2,21 @@
 
 void Galactic_Empires::perform_game_tick() {
 
-	/* Get input from all players */
-
-	// uint16_t i;
-	// for (i = 0; i < num_loaded_players; i++) {
-	// 	perform_player(i);
-	// }
-
+	/* Check if the game engine needs to run */
 	if(perform_year == false){
 		return;
 	}
 	perform_year = false;
-	
 	year++;
-	printf("Acting on year %lu\n", year);
-
-	/* Commit player actions */
 
 	uint64_t d, a, w;
 	/* Move all fleets */
-	//printf("Moving Fleets\n");
-	//fflush(stdout);
 	for (d = 0; d < all_fleets.size(); d++) {
-		//printf("Moving fleet %llu\n", d);
 		all_fleets[d].perform_fleet_tick();
 	}
 
 	/* Have each world build up its fleet */
-	//printf("Building Fleets\n");
 	for (w = 0; w < all_worlds.size(); w++) {
-		//printf("Looking at World %llu\n", w);
-		//fflush(stdout);
 
 		/* Special case for planets that can't build */
 		if(all_worlds[w].get_ship_building_capacity() == 0){
@@ -44,26 +28,18 @@ void Galactic_Empires::perform_game_tick() {
 		all_fleets[d].add_ships(all_worlds[w].get_ship_building_capacity());
 	}
 
-	// print_stats();
-
 	/* Have Fleets Battle */
 	/* Look at every world */
-	//printf("Checking for Battles\n");
-	//fflush(stdout);
 	for (w = 0; w < all_worlds.size(); w++) {
-		//printf("Looking at World %llu\n", w);
-		//fflush(stdout);
 		/* Find the fleet that is stationed here */
 		d = fleet_stationed(w);
 
 		/* Find any incoming attacking fleet */
 		for (a = 0; a < all_fleets.size(); a++) {
-			//printf("Looking at Attacking Fleet %llu\n", a);
-			//fflush(stdout);
-			//printf("Owner %d TT %llu\n World %d\n", all_fleets[d].get_owner(), all_fleets[d].get_travel_time_left(), all_worlds[w].get_owner());
 			if ((all_fleets[a].get_target() == all_worlds[w].get_position()) &&
 				(all_fleets[a].get_owner() != all_worlds[w].get_owner()) && 
 				(all_fleets[a].get_travel_time_left() == 0)) {
+
 				/* Battle! */
 				if (!all_fleets[d].engage_fleet(all_fleets[a])) {
 					/* If the defending fleet lost change the world owner */
